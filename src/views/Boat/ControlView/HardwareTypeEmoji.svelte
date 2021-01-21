38ebbd8a30c891b50b8d18ceaf96bf9b32787a37
Adrian <hannsadrian@gmail.com>
2021-01-21 22:30:31 +0100

    Move legacy frontend out of CosmicSail

---
+++ src/views/Boat/ControlView/HardwareTypeEmoji.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {onMount} from "svelte"
+
+    export let hardwareType;
+    let emoji = "";
+
+    onMount(() => {
+        switch (hardwareType) {
+            case "rudder":
+                emoji = "🛶"
+                break
+            case "sail":
+                emoji = "⛵"
+                break
+            case "engine":
+                emoji = "🔌"
+                break
+            case "gps":
+                emoji = "🛰"
+                break
+            case "bandwidth":
+                emoji = "📡"
+                break
+            case "ip":
+                emoji = "👾"
+                break
+        }
+    })
+</script>
+
+{emoji}
